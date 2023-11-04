b36a38a437971136e267c48cebd6185f522996e9
shubham dahiya <dahiya.shubham1234@gmail.com>
2023-11-04 21:29:25 +0530

    m

--- frontend/src/utils/getApiBaseUrl.js
+++ frontend/src/utils/getApiBaseUrl.js
@@ -1,7 +1,6 @@
-const getApiBaseUrl = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return 'http://localhost:8000';
-  }
-  return import.meta.env.VITE_API_BASE_URL;
-};
+const getApiBaseUrl = () =>
+  // if (process.env.NODE_ENV === 'development') {
+  //   return 'http://localhost:8000/';
+  // }
+  import.meta.env.VITE_API_BASE_URL;
 export default getApiBaseUrl;
